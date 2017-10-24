505961e042261ac3e8a86f99e53abcea6f04d545
Jasmine Turner <jazzturn@gmail.com>
2017-10-24 15:04:40 -0500

    added var and log info for students tab

--- main.js
+++ main.js
@@ -12,3 +12,10 @@ console.log("Ratings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
 avgRating = (ratings[1] + ratings[2] + ratings [3]) / 3;
 console.log("Teacher: " + teacher + "/nDeparment: " + department + "/nRatings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
 //challenge #4
+var email = "sally@cg.edu";
+var gpa = 6.0;
+var courses = ["Calculus", "Econ 101"];
+
+console.log("Email: " + email);
+console.log("GPA: " + gpa);
+console.log("Courses: " + courses[0] + " " + courses[1]);
