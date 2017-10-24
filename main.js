ea542d3a49174ea0371ffabc4b1f5dc394d141df
Jasmine Turner <jazzturn@gmail.com>
2017-10-24 14:29:56 -0500

    created variables and logged each variable

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("This file is linked!");
+var teacher = "Teacher: Jasmine Turner";
+var department = "Department: Music";
+var ratings = [4.2, 3.8, "2.0"];
+
+console.log(teacher);
+console.log(department);
+console.log("Ratings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
