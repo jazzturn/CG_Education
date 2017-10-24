99b4e022584aeb6a684c5bbd5c818e44c390989f
Jasmine Turner <jazzturn@gmail.com>
2017-10-24 14:47:31 -0500

    created an avgRatings var and formula and logged to console

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+//challenge #1 created and linked main.js file
+//challenge #2
 console.log("This file is linked!");
-var teacher = "Teacher: Jasmine Turner";
-var department = "Department: Music";
-var ratings = [4.2, 3.8, "2.0"];
+var teacher = "Teacher: Sally Jones";
+var department = "Department: Physics";
+var ratings = [4.2, 3.8, 2.0];
 
 console.log(teacher);
 console.log(department);
 console.log("Ratings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
+//challenge #3
+avgRating = (ratings[1] + ratings[2] + ratings [3]) / 3;
+console.log("Teacher: " + teacher + "/nDeparment: " + department + "/nRatings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
+//challenge #4
