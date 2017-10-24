c1acefa5f1320ba3e68ff470cbde000c064185b2
Jasmine Turner <jazzturn@gmail.com>
2017-10-24 15:34:37 -0500

    logged courses page to console using arrays

--- main.js
+++ main.js
@@ -12,10 +12,22 @@ console.log("Ratings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
 avgRating = (ratings[1] + ratings[2] + ratings [3]) / 3;
 console.log("Teacher: " + teacher + "/nDeparment: " + department + "/nRatings: " + ratings[0] + ", " + ratings[1] + ", " + ratings[2]);
 //challenge #4
+var student = "Sally Jones";
 var email = "sally@cg.edu";
 var gpa = 6.0;
 var courses = ["Calculus", "Econ 101"];
 
+console.log(student);
 console.log("Email: " + email);
 console.log("GPA: " + gpa);
 console.log("Courses: " + courses[0] + " " + courses[1]);
+
+var student = ["Jasmine Turner", "Sally Jones", "Michael Jordan"];
+var teacher = ["Sally Jones", "Jasmine Turner", "Whitney Houston"];
+var courses = ["Physics", "Astronomy", "StinkMath"];
+var semester = ["Fall 2017", "Spring 2018"];
+
+console.log("Student: " + student[0]);
+console.log("Teacher: " + teacher[2]);
+console.log("Course: " + courses[1]);
+console.log(semester[0]);
